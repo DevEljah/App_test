a53cbe1c6c8b6b1f65667b2c64c4862560dc12ca
Jahid Eljah <jahideljah@gmail.com>
2022-11-13 18:48:58 +0100

    some js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Hi");
+function count() {
+  for (let step = 0; step < 5; step++) {
+    // Runs 5 times, with values of step 0 through 4.
+    console.log("Walking east one step");
+  }
+}
+
+count();
